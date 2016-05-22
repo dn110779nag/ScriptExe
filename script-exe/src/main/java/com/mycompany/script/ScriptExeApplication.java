2f6caca959506ce7ca0ce05a97750cdc9dc55079
Aleksandr Novomlinov <dn110779nag@gmail.com>
2016-05-22 21:13:51 +0300

    Создал веб приложение

---
+++ script-exe/src/main/java/com/mycompany/script/ScriptExeApplication.java
@@ -0,0 +1,14 @@
+package com.mycompany.script;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@EnableAutoConfiguration
+public class ScriptExeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScriptExeApplication.class, args);
+	}
+}
