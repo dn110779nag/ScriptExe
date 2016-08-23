b69b863a0663367cc91d07418a4a3e08ed546e99
nova <nova@nova-All-Series>
2016-08-23 14:32:54 +0300

    Прикрутил версию правильно

--- script-exe/src/main/java/com/mycompany/script/ScriptExeApplication.java
+++ script-exe/src/main/java/com/mycompany/script/ScriptExeApplication.java
@@ -1,11 +1,9 @@
 package com.mycompany.script;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-//@EnableAutoConfiguration
 public class ScriptExeApplication {
 
     public static void main(String[] args) {
--- script-exe/src/main/java/com/mycompany/script/controllers/IndexController.java
+++ script-exe/src/main/java/com/mycompany/script/controllers/IndexController.java
@@ -6,6 +6,14 @@
 package com.mycompany.script.controllers;
 
 
+
+
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.info.ProjectInfoProperties;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,9 +26,13 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class IndexController {
+    
+    @Autowired
+    private BuildProperties buildProperties;
+    
     @RequestMapping({"/"})
     public ModelAndView returnIndex(@RequestParam(value="name", required = false) String name){
-        ModelAndView m = new ModelAndView("index", "name", name);
+        ModelAndView m = new ModelAndView("index", "name", name+buildProperties.getVersion()+"-"+buildProperties.getTime());
         
         return m;
     }
