7f4e996077bfa5cc3d2329c9a07549aa62b6045b
Samsung <Samsung@novax00-samsung-pc>
2016-10-01 10:40:30 +0300

    Добавил кеширование статики и починил h2 консоль

--- script-exe/src/main/java/com/mycompany/script/config/SecurityConfig.java
+++ script-exe/src/main/java/com/mycompany/script/config/SecurityConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -47,8 +49,24 @@ public class SecurityConfig  extends WebSecurityConfigurerAdapter{
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
+                
                 .jdbcAuthentication()
                 .dataSource(dataSource)
                 .passwordEncoder(passwordEncoder);
     }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.headers().cacheControl().
+//    }
+    
+    
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        
+        web.ignoring().antMatchers("/js/**","/h2/**","/images/**");
+    }
+    
+    
 }
--- script-exe/src/main/java/com/mycompany/script/controllers/AppErrorController.java
+++ script-exe/src/main/java/com/mycompany/script/controllers/AppErrorController.java
@@ -7,13 +7,11 @@ package com.mycompany.script.controllers;
 
 import com.mycompany.script.dto.AnswerDto;
 import java.util.Date;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.boot.autoconfigure.web.BasicErrorController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.web.ErrorController;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
@@ -26,6 +24,7 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("${server.error.path:${error.path:/error}}")
 public class AppErrorController implements ErrorController{
     private static final String ERROR_PATH = "/error";
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Override
     public String getErrorPath() {
